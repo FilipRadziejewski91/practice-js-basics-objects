c4a872d738063abb621711c5f0cea93d18667b4c
Filip Radziejewski <hugoboss2048@gmail.com>
2023-04-05 20:40:43 +0200

    complete task4

--- 04/app.js
+++ 04/app.js
@@ -2,8 +2,30 @@ const user = {
     firstName: 'Adam',
     lastName: 'Nowak',
     born: {
-        day: '14',
+        day: '05',
         month: '04',
         year: '1985'
     }
 }
+
+const now = new Date();
+// console.log(now);
+const nowDay = now.getDate();
+// console.log(nowDay);
+const nowMonth = now.getMonth();
+// console.log(nowMonth);
+
+const birthDay = parseInt(user.born.day);
+// console.log(birthDay);
+const birthMonth = parseInt(user.born.month);
+// console.log(birthMonth);
+
+const checkBirthday = function () {
+    if (birthDay === nowDay && birthMonth === nowMonth) {
+        console.log(user.firstName + " " + user.lastName + " obchodzi dziś urodziny!")
+    } else {
+        console.log(user.firstName + " " + user.lastName + " nie ma dziś urodzin :/")
+    }
+}
+
+checkBirthday();
